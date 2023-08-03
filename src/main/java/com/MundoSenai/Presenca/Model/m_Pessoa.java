ba50af17eb3937518fae4350893b4c3bfd5ebc18
hellenduarte05 <hellenduarte25@gmail.com>
2023-08-03 14:25:13 -0300

    atualizacao

--- src/main/java/com/MundoSenai/Presenca/Model/m_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Model/m_Pessoa.java
@@ -1,2 +1,73 @@
-package com.MundoSenai.Presenca.Model;public class m_Pessoa {
+package com.MundoSenai.Presenca.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Pessoa")
+public class m_Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private Long cpf;
+    private Long telefone;
+    private String datanasc;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getData_nasc() {
+        return datanasc;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.datanasc = data_nasc;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
--- src/main/java/com/MundoSenai/Presenca/Service/s_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Service/s_Pessoa.java
@@ -1,2 +1,18 @@
-package com.MundoSenai.Presenca.Service;public class s_Pessoa {
+package com.MundoSenai.Presenca.Service;
+
+import com.MundoSenai.Presenca.Model.m_Pessoa;
+import com.MundoSenai.Presenca.Repostory.R_Pessoa;
+import org.springframework.stereotype.Service;
+
+@Service
+public class s_Pessoa {
+    private static R_Pessoa r_Pessoa;
+
+    public s_Pessoa(R_Pessoa r_Pessoa){
+        this.r_Pessoa = r_Pessoa;
+    }
+
+    public static m_Pessoa getPessoaLogin(String usuario, String senha){
+        return r_Pessoa.findByUsuarioESenha(Long.valueOf(usuario),senha);
+    }
 }
